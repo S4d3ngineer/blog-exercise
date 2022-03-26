0762807b52d19ed475a6fb0b98e1b783eda9ab8b
Adam Arkuszyński <adam.arkuszynski@gmail.com>
2022-03-26 20:44:15 +0100

    replace postgres:// with postgresql://

--- main.py
+++ main.py
@@ -31,7 +31,10 @@ gravatar = Gravatar(app,
 
 # CONNECT TO DB
 # postgrade url is stored in env variable on heroku - if it is not found app is going to use sqlite db
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+postgres_url = os.environ.get("DATABASE_URL")
+if postgres_url.startswith("postgres://"):
+    postgres_url.replace("postgres://", "postgresql://")
+app.config['SQLALCHEMY_DATABASE_URI'] = postgres_url, "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
